bf7f8ce42412a8cd6531f96d7a062eeaa681abda
Robert Pirritano <rpirritano@gmail.com>
2018-03-31 07:50:19 -0700

    added nav links-style later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
 
+import {
+  routes as childRoutes,
+  ProductsModule
+} from './dashboard/dashboard.module';
 
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { DashboardContainerComponent } from './dashboard-container/dashboard-container.component';
-import { DashboardInvoiceNotifcationComponent } from './dashboard-invoice-notifcation/dashboard-invoice-notifcation.component';
-import { DashboardPosSummaryComponent } from './dashboard-pos-summary/dashboard-pos-summary.component';
-import { DashboardPosListComponent } from './dashboard-pos-list/dashboard-pos-list.component';
-
-
+//import { NavigationComponent } from './navigation/navigation.component';
+//import { DashboardContainerComponent } from './dashboard-container/dashboard-container.component';
+//import { DashboardInvoiceNotifcationComponent } from './dashboard-invoice-notifcation/dashboard-invoice-notifcation.component';
+//import { DashboardPosSummaryComponent } from './dashboard-pos-summary/dashboard-pos-summary.component';
+//import { DashboardPosListComponent } from './dashboard-pos-list/dashboard-pos-list.component';
+/*
+const routes: Routes = [
+  // basic routes
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashBoardComponent },
+  { path: 'exchange', component: ExchangeComponent },
+  { path: 'withdrawl', component: WithdrawalComponent },
+  { path: 'wallet', component: DashBoardComponent },
+  { path: 'taxes', component: TaxesComponent },
+  { path: 'pay-invoice', component: PayInvoiceComponent },
+  { path: 'send-invoice', component: SendInvoiceComponent },
+  { path: 'pos', component: PosComponent },
+  { path: 'payroll', component: PayrollComponent }
+  ];
+*/
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +41,10 @@ import { DashboardPosListComponent } from './dashboard-pos-list/dashboard-pos-li
     DashboardPosListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes), // <-- routes
   ],
   providers: [],
   bootstrap: [AppComponent]
